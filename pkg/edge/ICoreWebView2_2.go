8a74ff110379bf71ee56b0a7871274820f99d7e1
Eliohn <yi4534@qq.com>
2023-02-25 10:22:10 +0800

    ICoreWebView2_2 功能添加

--- pkg/edge/ICoreWebView2_2.go
+++ pkg/edge/ICoreWebView2_2.go
@@ -1,5 +1,7 @@
 package edge
 
+import "unsafe"
+
 type iCoreWebView2_2Vtbl struct {
 	iCoreWebView2Vtbl
 	AddWebResourceResponseReceived    ComProc
@@ -18,4 +20,20 @@ type ICoreWebView2_2 struct {
 func (i *ICoreWebView2_2) AddRef() uintptr {
 	r, _, _ := i.vtbl.AddRef.Call()
 	return r
+}
+
+func (i *ICoreWebView2) GetICoreWebView2_2() *ICoreWebView2_2 {
+	var result *ICoreWebView2_2
+
+	iidICoreWebView2_2 := NewGUID("{9E8F0CF8-E670-4B5E-B2BC-73E061E3184C}")
+	_, _, _ = i.vtbl.QueryInterface.Call(
+		uintptr(unsafe.Pointer(i)),
+		uintptr(unsafe.Pointer(iidICoreWebView2_2)),
+		uintptr(unsafe.Pointer(&result)))
+
+	return result
+}
+
+func (e *Chromium) GetICoreWebView2_2() *ICoreWebView2_2 {
+	return e.webview.GetICoreWebView2_2()
 }
